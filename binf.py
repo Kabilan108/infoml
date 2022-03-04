18d245f7ed68429d28c7bc123fe79c2d94e4a15d
Tony Okeke <tko35@drexel.edu>
2022-03-03 21:47:43 -0500

    Updated visible methods in module

--- binf.py
+++ binf.py
@@ -5,8 +5,8 @@ algorithms.
 """
 
 # Imports
-import Bio.Align as Align
-import numpy as np
+import Bio.Align as _Align
+import numpy as _np
 from typing import Union as _Union
 
 
@@ -24,14 +24,14 @@ class BWT:
 
     def inverse(sequence: str) -> str:
         table = [col for col in sequence]
-        for i in range(len(sequence) - 1):
+        for _ in range(len(sequence) - 1):
             table.sort()
             table = [sequence[i] + table[i] for i in range(len(sequence))]
 
         return table[[row[-1] for row in table].index('$')] 
 
 
-def swalign(a: str, b: str, gap: int=-5, submat: Align.substitution_matrices.Array=None,
+def swalign(a: str, b: str, gap: int=-5, submat: _Align.substitution_matrices.Array=None,
             scoreonly: bool=False, identonly: bool=False) -> _Union[int, float, dict]:
     """
     This is a custom implementation of the Smith-Waterman Local Sequence Alignment Algorithm.
@@ -54,12 +54,12 @@ def swalign(a: str, b: str, gap: int=-5, submat: Align.substitution_matrices.Arr
 
     # Default substitution matrix
     if submat is None:
-        submat = Align.substitution_matrices.load('BLOSUM62')
+        submat = _Align.substitution_matrices.load('BLOSUM62')
     # Define sequence lengths
     A = len(a)
     B = len(b)
     # Initialize Dynamic Programming (score) table
-    T = np.zeros( (A+1, B+1) ).tolist()
+    T = _np.zeros( (A+1, B+1) ).tolist()
 
     if scoreonly:
         for i in range(A):
@@ -72,12 +72,12 @@ def swalign(a: str, b: str, gap: int=-5, submat: Align.substitution_matrices.Arr
             for j in range(B):
                 # reset, diag, horz, vert.
                 Ti_plus1[j+1] = max( 0, Ti[j]+submat_ai[b[j]], Ti_plus1[j]+gap, Ti[j+1]+gap )
-        return np.max(T, axis=None)
+        return _np.max(T, axis=None)
 
     elif identonly:
         # Initialize counts
-        MC = np.zeros((A+1, B+1), dtype=int).tolist() # match counts
-        AL = np.zeros((A+1, B+1), dtype=int).tolist() # alignment lengths
+        MC = _np.zeros((A+1, B+1), dtype=int).tolist() # match counts
+        AL = _np.zeros((A+1, B+1), dtype=int).tolist() # alignment lengths
 
         for i in range(A):
             submat_ai = submat[a[i]]
@@ -103,7 +103,7 @@ def swalign(a: str, b: str, gap: int=-5, submat: Align.substitution_matrices.Arr
 
     else:
         # Store the best direction (we only keep one when there are multiple good options.)
-        P = np.zeros((A+1, B+1), dtype=int).tolist()
+        P = _np.zeros((A+1, B+1), dtype=int).tolist()
         for i in range(A):
             submat_ai = submat[a[i]]
             Ti = T[i]
@@ -113,12 +113,12 @@ def swalign(a: str, b: str, gap: int=-5, submat: Align.substitution_matrices.Arr
             for j in range(B):
                 # reset, diag, horz, vert.
                 options = ( 0, Ti[j]+submat_ai[b[j]], Ti_plus1[j]+gap, Ti[j+1]+gap )
-                bestmove = np.argmax(options)
+                bestmove = _np.argmax(options)
                 Ti_plus1[j+1] = options[bestmove]
                 Pi_plus1[j+1] = bestmove
 
     # Determine score positions
-    scorepos = np.unravel_index(np.argmax(T, axis=None), (A+1, B+1))
+    scorepos = _np.unravel_index(_np.argmax(T, axis=None), (A+1, B+1))
     r,c = scorepos # r=scorepos[0]; c=scorepos[1]
     if identonly:
         # Compute and return percent identity
@@ -154,6 +154,6 @@ def swalign(a: str, b: str, gap: int=-5, submat: Align.substitution_matrices.Arr
 
     # Compute percent identity
     L = len(align[0])
-    ident = np.count_nonzero([align[0][i] == align[1][i] for i in range(L)]) / L * 100
+    ident = _np.count_nonzero([align[0][i] == align[1][i] for i in range(L)]) / L * 100
 
     return {'score': score, 'align': align, 'ident': ident}
