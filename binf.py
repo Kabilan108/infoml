4cb145657889023143b19ceacc3221a4848644f0
Tony Okeke <tko35@drexel.edu>
2022-07-05 23:32:05 -0400

    Made usability improvements to functions

--- binf.py
+++ binf.py
@@ -5,21 +5,22 @@ algorithms.
 """
 
 # Imports
+import ToolBox.utils as utils
 import Bio.Align as Align
 import pandas as pd
 import numpy as np
+import subprocess
 import GEOparse
+import requests
 import sqlite3
 import pickle
-import utils
+import re
 import os
-from utils import download
+from ToolBox.utils import download
 from typing import Union
 
+# TODO: Function to generate phylogenetic tree from distance matrix (turtle)
 
-# Definitions
-#TODO: Write function to generate phylogenetic tree from distance matrix (use turtle)
-#TODO: use turtle to draw tree as you condense the distance matrix (use arithmetic mean)
 
 class BWT:
     """
@@ -27,10 +28,11 @@ class BWT:
     """
     def transform(sequence: str) -> str:
         sequence += '$'
-        table = [sequence[index:] + sequence[:index] for index, _ in enumerate(sequence)]
+        table = [sequence[index:] + sequence[:index]
+            for index, _ in enumerate(sequence)]
         table.sort()
-	    
-        return ''.join([ rotation[-1] for rotation in table ])
+
+        return ''.join([rotation[-1] for rotation in table])
 
     def inverse(sequence: str) -> str:
         table = [col for col in sequence]
@@ -38,15 +40,18 @@ class BWT:
             table.sort()
             table = [sequence[i] + table[i] for i in range(len(sequence))]
 
-        return table[[row[-1] for row in table].index('$')] 
+        return table[[row[-1] for row in table].index('$')]
 
 
-def swalign(a: str, b: str, gap: int=-5, submat: Align.substitution_matrices.Array=None,
-            scoreonly: bool=False, identonly: bool=False) -> Union[int, float, dict]:
+def swalign(a: str, b: str, gap: int = -5, scoreonly: bool = False,
+            submat: Align.substitution_matrices.Array = None,
+            identonly: bool = False) -> Union[int, float, dict]:
     """
-    This is a custom implementation of the Smith-Waterman Local Sequence Alignment Algorithm.
-    It is based on a code written by Dr. Ahmet Sacan <ahmetmsacan@gmail.com>
-    It uses the Dynamic Programming algorithm to obtain an optimal sequence alignment.
+    This is a custom implementation of the Smith-Waterman Local Sequence
+    Alignment Algorithm.
+    Based on a code written by Dr. Ahmet Sacan <ahmetmsacan@gmail.com>
+    Uses the Dynamic Programming algorithm to obtain an optimal sequence
+    alignment.
     This function only supports linear gap penalties.
     @param a: str, b: str
         Sequences to align
@@ -67,68 +72,72 @@ def swalign(a: str, b: str, gap: int=-5, submat: Align.substitution_matrices.Arr
         submat = Align.substitution_matrices.load('BLOSUM62')
     # Define sequence lengths
     A = len(a)
-    B = len(b) 
+    B = len(b)
     # Initialize Dynamic Programming (score) table
-    T = np.zeros( (A+1, B+1) ).tolist()
+    T = np.zeros((A + 1, B + 1)).tolist()
 
     if scoreonly:
         for i in range(A):
             # Define variables that reference positions in the score table
-            # This is a speed optimization - directly reference position instead of searching
-            # for it each time
+            # This is a speed optimization - directly reference position
+            # instead of searching for it each time
             submat_ai = submat[a[i]]
             Ti = T[i]
-            Ti_plus1 = T[i+1]
+            Ti_plus1 = T[i + 1]
             for j in range(B):
                 # reset, diag, horz, vert.
-                Ti_plus1[j+1] = max( 0, Ti[j]+submat_ai[b[j]], Ti_plus1[j]+gap, Ti[j+1]+gap )
+                Ti_plus1[j + 1] = max(0, Ti[j] + submat_ai[b[j]],
+                                      Ti_plus1[j] + gap, Ti[j + 1] + gap )
         return np.max(T, axis=None)
 
     elif identonly:
         # Initialize counts
-        MC = np.zeros((A+1, B+1), dtype=int).tolist() # match counts
-        AL = np.zeros((A+1, B+1), dtype=int).tolist() # alignment lengths
+        MC = np.zeros((A + 1, B + 1), dtype=int).tolist() # match counts
+        AL = np.zeros((A + 1, B + 1), dtype=int).tolist() # alignment lengths
 
         for i in range(A):
             submat_ai = submat[a[i]]
-            Ti = T[i];	Ti_plus1 = T[i+1];
-            MCi = MC[i];	MCi_plus1 = MC[i+1];
-            ALi = AL[i];	ALi_plus1 = AL[i+1];
+            Ti = T[i];	Ti_plus1 = T[i + 1];
+            MCi = MC[i];	MCi_plus1 = MC[i + 1];
+            ALi = AL[i];	ALi_plus1 = AL[i + 1];
 
             for j in range(B):
                 # reset, diag, horz, vert.
-                options = ( 0, Ti[j]+submat_ai[b[j]], Ti_plus1[j]+gap, Ti[j+1]+gap )
+                options = (0, Ti[j] + submat_ai[b[j]], Ti_plus1[j] + gap, 
+                           Ti[j+1] + gap)
                 bestmove = np.argmax(options)
-                Ti_plus1[j+1] = options[bestmove]
+                Ti_plus1[j + 1] = options[bestmove]
 
                 if bestmove == 1: # Diagonal
-                    MCi_plus1[j+1] = MCi[j] + (a[i] == b[j])
-                    ALi_plus1[j+1] = ALi[j] + 1
+                    MCi_plus1[j + 1] = MCi[j] + (a[i] == b[j])
+                    ALi_plus1[j + 1] = ALi[j] + 1
                 elif bestmove == 2: # Horizontal
-                    MCi_plus1[j+1] = MCi_plus1[j]
-                    ALi_plus1[j+1] = ALi_plus1[j] + 1
+                    MCi_plus1[j + 1] = MCi_plus1[j]
+                    ALi_plus1[j + 1] = ALi_plus1[j] + 1
                 elif bestmove == 3: # Vertical
-                    MCi_plus1[j+1] = MCi[j+1]
-                    ALi_plus1[j+1] = ALi[j+1] + 1
+                    MCi_plus1[j + 1] = MCi[j + 1]
+                    ALi_plus1[j + 1] = ALi[j + 1] + 1
 
     else:
-        # Store the best direction (we only keep one when there are multiple good options.)
-        P = np.zeros((A+1, B+1), dtype=int).tolist()
+        # Store the best direction (we only keep one when there are multiple 
+        # good options.)
+        P = np.zeros((A + 1, B + 1), dtype=int).tolist()
         for i in range(A):
             submat_ai = submat[a[i]]
             Ti = T[i]
-            Ti_plus1 = T[i+1]
-            Pi_plus1 = P[i+1]
+            Ti_plus1 = T[i + 1]
+            Pi_plus1 = P[i + 1]
 
             for j in range(B):
                 # reset, diag, horz, vert.
-                options = ( 0, Ti[j]+submat_ai[b[j]], Ti_plus1[j]+gap, Ti[j+1]+gap )
+                options = (0, Ti[j] + submat_ai[b[j]], Ti_plus1[j] + gap, 
+                           Ti[j + 1] + gap)
                 bestmove = np.argmax(options)
-                Ti_plus1[j+1] = options[bestmove]
-                Pi_plus1[j+1] = bestmove
+                Ti_plus1[j + 1] = options[bestmove]
+                Pi_plus1[j + 1] = bestmove
 
     # Determine score positions
-    scorepos = np.unravel_index(np.argmax(T, axis=None), (A+1, B+1))
+    scorepos = np.unravel_index(np.argmax(T, axis=None), (A + 1, B + 1))
     r,c = scorepos # r=scorepos[0]; c=scorepos[1]
     if identonly:
         # Compute and return percent identity
@@ -170,8 +179,9 @@ def swalign(a: str, b: str, gap: int=-5, submat: Align.substitution_matrices.Arr
 
 
 def nwalign(a: str, b: str, match: int=1, mismatch: int=-1, gap: int=-2,
-            score_only: bool=False, ident_only: bool=False, penalize_end_gaps=False,
-            submat: Align.substitution_matrices.Array=None, alphabet: str='nt') -> Union[int, float, dict]:
+            score_only: bool=False, ident_only: bool=False, alphabet: str='nt',
+            submat: Align.substitution_matrices.Array=None,
+            penalize_end_gaps=False) -> Union[int, float, dict]:
     """
     Custom implementation of the Needleman-Wunsch algorithm
     """
@@ -210,18 +220,25 @@ def nwalign(a: str, b: str, match: int=1, mismatch: int=-1, gap: int=-2,
                     Ti_plus1[j+1] = max( Ti[j]+submat_ai[b[j]], Ti_plus1[j]+gap, Ti[j+1]+gap )
             return np.hstack([np.array(T)[A, :], np.array(T)[:, B]]).max(axis=None)
 
-def geodlparse(acc: str):
+def geodlparse(acc: str, limit_runs: int=1):
     """
     Download, parse and cache data from GEO
 
     @param acc
         GEO accession
+    @param limit_runs
+        Number of runs to retrieve for each SRX
     @return
         parsed GEO data
     """
 
     # Path to temporary directory
     geodir = utils.tempdir('GEO')
+    fastqdir = "/mnt/e/data/fastq"  # Path to fastq directory
+
+    # Paths to command-line tools
+    PREFETCH = "/home/kabil/.anaconda3/envs/sra/bin/prefetch"
+    FASTQDUMP = "/home/kabil/.anaconda3/envs/sra/bin/fasterq-dump"
 
     # Download files
     try:
@@ -236,7 +253,6 @@ def geodlparse(acc: str):
                 print('Loading cached data...')
                 with open(cachefile, 'rb') as cache:
                     geodata =  pickle.load(cache)
-                return geodata
             except Exception as e:
                 print(f"ERROR: Loading cached file failed.\n{e}")
         else:
@@ -251,9 +267,36 @@ def geodlparse(acc: str):
             # Cache data
             with open(cachefile, 'wb') as cache:
                 pickle.dump(geodata, file=cache)
-            return geodata
     except Exception as e:
-        print(f"ERROR: Enter a valid GEO Accension\n{e}")
+        print(f"ERROR: Enter a valid GEO Accession\n{e}")
+
+    if acc.startswith('GSE') and re.search(r'hi.* thr.* seq.*', geodata.metadata['type'][0]):
+        # # Get run accessions for each GSM in the series
+        # srp = re.search(r'(?<=term=).*', geodata.relations['SRA'][0])[0]
+        # r = requests.get(f"https://api.omicidx.cancerdatasci.org/sra/studies/{srp}/runs")
+        # r.raise_for_status()
+
+        # runs = {gsm: [] for gsm in geodata.phenotype_data.index}
+        # for hit in r.json()['hits']:
+        #     runs[ hit['sample']['alias'] ].append( hit['accession'] )
+
+        # # Limit the number of runs to retrieve
+        # for gsm in runs:
+        #     runs[gsm] = runs[gsm][:limit_runs]
+
+        # # Download .sra files
+        # sra_nums = [i for sub in runs.values() for i in sub]
+        # for sra_id in sra_nums:
+        #     cmd = f'{PREFETCH} {sra_id}'
+        #     subprocess.call(cmd, shell=True)
+
+        # Check
+
+        raise NotImplementedError
+        # print(runs)
+
+    return geodata
+
 
 def targetscandb(mirna: str, scorethr: float=0.8, db: str='mir2target'):
     """
