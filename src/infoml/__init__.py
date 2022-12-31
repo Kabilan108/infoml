95f36e152e7cc23e4a330093af867aa48d930991
Tony Okeke <tonykabilanokeke@gmail.com>
2022-12-31 10:58:31 -0500

    Add __init__

--- src/infoml/__init__.py
+++ src/infoml/__init__.py
@@ -1,3 +1,25 @@
-# read version from installed package
+"""
+infoml
+======
+
+infoml is a Python package for perfoming bioinformatics analysis and machine learning
+tasks on genomic and bioimaging data.
+"""
+
+# TODO: Create CONFIG class
+#   Create a dataclass that will store configuration information for the package
+
+class dirs:
+    DATADIR = "/home/kabil/.data"
+    TEMPDIR = "/tmp/data/"
+
+# TODO: Create a function to auto clean any cache files
+
+# FEAT: Make modules accessible from the main module
+
+# Define package version
 from importlib.metadata import version
 __version__ = version("infoml")
+
+# Define visible modules
+__all__ = ['analysis', 'binf', 'utils', 'neuro']
