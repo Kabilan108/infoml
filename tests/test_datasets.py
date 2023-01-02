bd7d7662ef23a1ff3b9d1a51c8cd561cae25dec0
Tony Okeke <tonykabilanokeke@gmail.com>
2023-01-02 07:33:43 -0500

    refactor: Refactor with Black

--- tests/test_datasets.py
+++ tests/test_datasets.py
@@ -5,6 +5,7 @@ Tests for `infoml.datasets` module.
 import pytest
 import gzip
 
+
 def test_get_ceman_counts():
     """
     Test the function `get_ceman_counts`.
@@ -16,4 +17,3 @@ def test_get_ceman_counts():
     with gzip.open(get_ceman_counts()) as f:
         lines = f.readlines()
     assert len(lines) == 23369
-    
