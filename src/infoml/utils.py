578fc574f71450ccab11c93e4162851f87f997d0
Tony Okeke <tonykabilanokeke@gmail.com>
2023-01-02 10:59:56 -0500

    feat: Define I/O for utils module

--- src/infoml/utils.py
+++ src/infoml/utils.py
@@ -15,3 +15,24 @@ from tqdm.auto import tqdm
 from rich import print
 import requests
 
+# Imports from local source
+from .utils import CONFIG
+
+
+# Define module I/O
+__all__ = [
+    "ispc",
+]
+__all__ += [m for m in dir() if m.startswith("__")]
+
+
+def __dir__():
+    """Override default dir() behavior"""
+    return __all__
+
+
+def __getattr__(name):
+    """Override default getattr() behavior"""
+    if name not in __all__:
+        raise AttributeError(f"module {__name__!r} has no attribute {name!r}")
+    return globals()[name]
