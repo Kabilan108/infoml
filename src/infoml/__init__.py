aecc4f24d5aba1fd4f1416e54457cf6e98306895
Tony Okeke <tonykabilanokeke@gmail.com>
2023-01-02 09:38:52 -0500

    feat: Add config class with paths to data directories

--- src/infoml/__init__.py
+++ src/infoml/__init__.py
@@ -2,12 +2,92 @@
 infoml
 ------
 
-infoml is a Python package for perfoming bioinformatics analysis and machine learning
-tasks on genomic and bioimaging data.
+`infoml` is a Python package for perfoming bioinformatics analysis and machine 
+learning tasks on genomic and bioimaging data.
 """
 
-# TODO: Create CONFIG class
-#   Create a dataclass that will store configuration information for the package
+# Import modules
+from pathlib import Path
+import tempfile
+
+
+class config:
+    """
+    Configuration class for infoml package
+
+    Methods
+    -------
+    cache : pathlib.PosixPath
+        Path to the cache directory. This will be the default location for
+        storing cached data.
+    datadir : pathlib.PosixPath
+        Path to the data directory. This will be the default location for
+        storing downloaded data.
+    tempdir : pathlib.PosixPath
+        Path to the temporary directory. This will be the default location for
+        storing temporary files.
+
+    Examples
+    --------
+    >>> from infoml import CONFIG
+    >>> CONFIG.DATADIR
+    PosixPath('/home/kabil/.data/infoml')
+    >>> # Change the default data directory
+    >>> CONFIG.DATADIR = "./data"
+    """
+
+    __cache = Path.home() / ".cache" / "infoml"
+    __datadir = Path.home() / ".data" / "infoml"
+    __tempdir = Path(tempfile.gettempdir()) / "infoml"
+
+
+    def __init__(self):
+        """Initialize the configuration class"""
+
+        if not self.__cache.exists():
+            self.__cache.mkdir(parents=True)
+
+        if not self.__datadir.exists():
+            self.__datadir.mkdir(parents=True)
+
+        if not self.__tempdir.exists():
+            self.__tempdir.mkdir(parents=True)
+
+
+    def cache(self, new_path: Path | str=""):
+        """Cache directory"""
+
+        if new_path:
+            self.__cache = Path(new_path)
+
+            if not self.__cache.exists():
+                self.__cache.mkdir(parents=True)
+
+        return self.__cache
+
+
+    def datadir(self, new_path: Path | str=""):
+        """Data directory"""
+
+        if new_path:
+            self.__datadir = Path(new_path)
+
+            if not self.__datadir.exists():
+                self.__datadir.mkdir(parents=True)
+
+        return self.__datadir
+
+
+    def tempdir(self, new_path: Path | str=""):
+        """Temporary directory"""
+
+        if new_path:
+            self.__tempdir = Path(new_path)
+
+            if not self.__tempdir.exists():
+                self.__tempdir.mkdir(parents=True)
+
+        return self.__tempdir
 
 
 class dirs:
