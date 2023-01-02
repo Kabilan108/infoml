05d039215a2a049787e2811f741a0a317a9c35d8
Tony Okeke <tonykabilanokeke@gmail.com>
2023-01-02 08:17:38 -0500

    docs: update header

--- src/infoml/datasets.py
+++ src/infoml/datasets.py
@@ -2,6 +2,7 @@
 datasets
 --------
 
+This module contains functions to get the paths to the datasets used in the
 """
 
 # Imports
@@ -10,7 +11,7 @@ from importlib import resources
 
 def get_ceman_counts():
     """
-    Get path the GSE50499 [1] gene counts matrix.
+    Get path the GSE50499 [1]_ gene counts matrix.
 
     Returns
     -------
@@ -19,9 +20,9 @@ def get_ceman_counts():
 
     References
     ----------
-    .. [1] Kenny PJ, Zhou H, Kim M, Skariah G et al. MOV10 and FMRP regulate
-    AGO2 association with microRNA recognition elements. Cell Rep 2014 Dec
-    11;9(5):1729-1741. PMID: 25464849
+    .. [1] Kenny PJ, Zhou H, Kim M, Skariah G et al. MOV10 and FMRP regulate 
+           AGO2 association with microRNA recognition elements. Cell Rep 2014 
+           Dec 11;9(5):1729-1741. PMID: 25464849
     """
 
     with resources.path("infoml.data", "GSE50499_GEO_Ceman_counts.txt.gz") as f:
