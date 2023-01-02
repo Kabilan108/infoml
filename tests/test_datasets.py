03ca62fae449c9dcc71bbaf691ed56431d96f755
Tony Okeke <tonykabilanokeke@gmail.com>
2023-01-02 07:29:41 -0500

    test: Add test for datasets module

---
+++ tests/test_datasets.py
@@ -0,0 +1,19 @@
+"""
+Tests for `infoml.datasets` module.
+"""
+
+import pytest
+import gzip
+
+def test_get_ceman_counts():
+    """
+    Test the function `get_ceman_counts`.
+    """
+    from infoml.datasets import get_ceman_counts
+
+    assert get_ceman_counts().exists()
+
+    with gzip.open(get_ceman_counts()) as f:
+        lines = f.readlines()
+    assert len(lines) == 23369
+    
